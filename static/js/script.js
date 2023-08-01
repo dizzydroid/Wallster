fe67b16c37c7555abdf6d40acf312a2650f66340
dizzydroid <shehabmahmoud2003@gmail.com>
2023-08-01 12:03:31 +0300

    commits

--- static/js/script.js
+++ static/js/script.js
@@ -26,8 +26,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
-    
+    let switcher = document.querySelector('#themeSwitch');
 
+    switcher.addEventListener('change', function(event) {
+        if (event.target.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light');
+        }
+    });
+
+    // Retrieve from local storage
+    var savedTheme = localStorage.getItem('theme') || 'light';
+
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        switcher.checked = true;
+    } else {
+        document.body.classList.remove('dark-mode');
+        switcher.checked = false;
+    }
 });
 
 function copyToClipboard(text) {
@@ -39,3 +59,59 @@ function copyToClipboard(text) {
         console.error('Could not copy text: ', error);
     });
 }
+
+let sunIcon = document.querySelector('#sunIcon');
+let moonIcon = document.querySelector('#moonIcon');
+
+switcher.addEventListener('change', function(event){
+    if (event.target.checked) {
+        document.body.classList.add('dark-theme');
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    } else {
+        document.body.classList.remove('dark-theme');
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
+    }
+});
+
+window.addEventListener('load', function() {
+    let logo = document.querySelector('#logo');
+    let lightLogoUrl = logo.dataset.light;
+    let darkLogoUrl = logo.dataset.dark;
+
+    let sunIcon = document.getElementById('sunIcon');
+    let moonIcon = document.getElementById('moonIcon');
+
+    let switcher = document.querySelector('.theme-switch input[type="checkbox"]');
+
+    switcher.addEventListener('change', function(event){
+        if (event.target.checked) {
+            document.body.classList.add('dark-theme');
+            sunIcon.style.display = 'none';
+            moonIcon.style.display = 'inline-block';
+            logo.src = darkLogoUrl;
+        } else {
+            document.body.classList.remove('dark-theme');
+            sunIcon.style.display = 'inline-block';
+            moonIcon.style.display = 'none';
+            logo.src = lightLogoUrl;
+        }
+    });
+
+    // Initialize theme based on saved setting
+    var savedTheme = localStorage.getItem('theme') || 'light';
+    document.body.setAttribute('data-theme', savedTheme);
+    switcher.checked = savedTheme === 'dark';
+
+    if(switcher.checked){
+        logo.src = darkLogoUrl;
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    } else {
+        logo.src = lightLogoUrl;
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
+    }
+
+});
